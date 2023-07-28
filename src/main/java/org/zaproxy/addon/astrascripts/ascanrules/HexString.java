d4f0afae9dac603feeebeac86d09a4dbc0ca8cff
Aryan Gupta <87857234+aryangupta701@users.noreply.github.com>
2023-07-28 01:03:07 -0700

    updated packages name and imports (#3)
    Signed-off-by: aryangupta701 <garyan447@gmail.com>

--- src/main/java/org/zaproxy/addon/astrascripts/ascanrules/HexString.java
+++ src/main/java/org/zaproxy/addon/astrascripts/ascanrules/HexString.java
@@ -18,7 +18,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.zaproxy.addon.astrascripts.ascanrules;
+package org.zaproxy.addon.astrascripts;
 
 import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
